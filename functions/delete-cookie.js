4287e0b958a938fbe75fd0434f191a01a01b9f63
DavidWells <hello@davidwells.io>
2018-07-11 14:03:15 -0700

    simple redirect

--- functions/delete-cookie.js
+++ functions/delete-cookie.js
@@ -4,6 +4,7 @@ import util from 'util'
 
 exports.handler = (event, context, callback) => {
 
+  console.log('event', event)
   const params = event.queryStringParameters || {}
   const redirectUrl = params.url
   const authToken = params.token
@@ -33,42 +34,12 @@ exports.handler = (event, context, callback) => {
     </head>
     <body>
       <noscript>
-        <meta http-equiv="refresh" content="0; url=${process.env.URL}" />
+        <meta http-equiv="refresh" content="0; url=${redirectUrl}" />
       </noscript>
     </body>
     <script>
-      function redirect(url) {
-        var dom = window.document.createElement('form');
-
-        var parts = url.split('?');
-        var url_ = parts[0], params = parts[1] || '';
-        var paramlist = params.split('&');
-
-        dom.setAttribute('method', 'get');
-        dom.setAttribute('action', url_);
-        dom.style.display = 'none';
-        dom.style.visibility = 'hidden';
-
-        var e, kv, k, v;
-        for (var i = 0; i < paramlist.length; ++i) {
-          kv = paramlist[i].split('=');
-          k = kv[0];
-          v = kv[1];
-          e = window.document.createElement('input');
-
-          e.setAttribute('type', 'hidden');
-          e.setAttribute('name', decodeURIComponent(k));
-          e.setAttribute('value', decodeURIComponent(v));
-
-          dom.appendChild(e);
-        }
-
-        window.document.body.appendChild(dom);
-        dom.submit();
-      }
-
       setTimeout(function(){
-        redirect('https://gated-sites-demo-login-site.netlify.com/')
+        window.location.href = ${JSON.stringify(redirectUrl)}
       }, 0)
     </script>
   </html>`;
